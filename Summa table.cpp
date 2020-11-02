2a2b7872809dfb9670064dd7b5784a008ca9e8a4
Bronnikova98 <bronnikowa-wika@mail.ru>
2020-11-02 17:51:05 +0500

    Добавила код для редактирования

---
+++ Summa table.cpp
@@ -0,0 +1,33 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+
+	double a, b, h, y, s, p;
+	int n, i, x;
+	
+	cin >> a >> b >> n;
+	h = (b - a);
+	x = a;
+	do
+	{
+		p = s = 1;
+		for (i = 1; i <= n; i++)
+		{
+			p *= pow((-1),i) * pow(x,2)/2*i;
+			s += p;
+		}
+		y = cos(x);
+		
+		x += h;
+	} 
+	while (x <= b + h/2);
+	cout << endl;
+	return 0;
+}
+
